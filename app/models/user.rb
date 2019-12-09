24c1fb658a828470a72c1fe0b30275fd6291b329
BOOKCHANG <book770208@gmail.com>
2019-12-09 12:16:57 +0800

    add google_oauth (WIP)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2] # 若要增加串接Facebook，則可寫 %i[google_oauth2 facebook]
   def employee?
     role.in? ['staff', 'boss', 'admin']
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user| # 查找出一個陣列裡包含provider及uid，若查無資料，則建立一個新的使用者
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.avatar = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
   end
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } # 當完成驗證後，回傳成功
   resources :books, only: [:index, :show]
   root 'books#index'
   
